4720637f834bf6a52f852bc681e19c3b29b8999b
Dan Serino <serino.daniel@gmail.com>
2017-04-29 22:25:46 -0400

    can download data and calculate mean and std dev

--- strategy/dataDownloadFunctions.py
+++ strategy/dataDownloadFunctions.py
@@ -1,12 +1,14 @@
 import urllib2
 
-def downloadHistoricData(Symbols, \
+def downloadHistoricalData(Symbols, \
                          year0,month0,day0, \
                          year1,month1,day1):
-    # download historical data from yahoo finance
-    # returns AllData
-    # AllData[k_symbol][k_date][k_column]
-    # date of quote, open, high, low, close, volume, adj close
+    # download historical data from yahoo finance given
+    # initial date and end date.
+    # AllData is returned in format:
+    #   - AllData[k_symbol][k_date][k_column]
+    # The columns are organized by
+    # [date of quote, open, high, low, close, volume, adj close]
     
     AllData = []
     
@@ -30,14 +32,21 @@ def downloadHistoricData(Symbols, \
 
         # for each row, split comma separated data into columns 
         data2 = []
+        # start from second row and 
         # loop to length-1 because last row will be blank
-        for n in range(0,len(data1)-1):
-            data2.append(data1[n].split(','))
+        for n in range(1,len(data1)-1):
+            tmp = data1[n].split(',')
+            
+            # now change appropriate numbers to 
+            for col in range(1,len(tmp)):
+                tmp[col] = eval(tmp[col])
+                             
+            data2.append(tmp)
 
         # append to all data
         AllData.append(data2)
 
-        print "["+str(k)+","+str(len(Symbols))+"]"
+        print "["+str(k+1)+"/"+str(len(Symbols))+"]"
 
     # return all data
     return AllData
